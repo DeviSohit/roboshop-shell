636346d19e2a87cdd2f4602868c840ce28e4b9e1
devi <devimeena.iiit@gmail.com>
2023-08-22 13:10:18 +0530

    creating MongoDB

--- mongodb.sh
+++ mongodb.sh
@@ -20,3 +20,21 @@ VALIDATE(){
     echo -e "$R ERROR:Please run this script with root access $N"
     exit 1
 fi
+
+cp mongo.repo /etc/yum.repos.d/mongo.repo &>> $LOGFILE
+VALIDATE $? "Creating & copying MongoDB repo file"
+
+yum install mongodb-org -y &>> $LOGFILE
+VALIDATE $? "Installing MongoDB"
+
+systemctl enable mongod &>> $LOGFILE
+VALIDATE $? "Enabling MongoDB"
+
+systemctl start mongod &>> $LOGFILE
+VALIDATE $? "Starting MongoDB"
+
+sed -i 's/127.0.0.1/0.0.0.0/' /etc/mongod.conf &>> $LOGFILE
+VALIDATE $? "Updating listen address"
+
+systemctl restart mongod &>> $LOGFILE
+VALIDATE $? "Restarting MongoDB"
