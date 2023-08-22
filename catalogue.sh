08af6a65dcfb945e06bfd4db01462ef5689e485f
devi <devimeena.iiit@gmail.com>
2023-08-22 14:23:51 +0530

    creating catalogue service

--- catalogue.sh
+++ catalogue.sh
@@ -45,7 +45,7 @@ VALIDATE $? "Extracting catalogue.zip in app directory"
 npm install &>> $LOGFILE
 VALIDATE $? "Downloading dependencies"
 
-cp catalogue.service /etc/systemd/system/catalogue.service &>> $LOGFILE
+cp catalogue.service /etc/systemd/system/catalogue.service 
 VALIDATE $? "Creating & copying System catalogue service"
 
 systemctl daemon-reload &>> $LOGFILE
