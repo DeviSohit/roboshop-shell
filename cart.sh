76f77f476580abf35f54509eb3295c0c57e9b25b
devi <devimeena.iiit@gmail.com>
2023-08-23 13:50:00 +0530

    creating cart

--- cart.sh
+++ cart.sh
@@ -31,12 +31,11 @@ VALIDATE $? "Installing NodeJs"
 USER=$(getent passwd | grep roboshop)
 echo $USER &>> $LOGFILE
 
-if [ $? -ne 0 ] ; then
-echo "add user"
+if [ $? -ne 0 ]; then
 useradd roboshop
 VALIDATE $? "Adding user"
-#else
-#echo -e "$Y user already exist $N"
+else
+echo -e "$Y user already exist $N"
 fi
 
 DIRECTORY=$(cd /app)
@@ -44,8 +43,8 @@ echo $DIRECTORY
 if [ $? -ne 0 ] ; then
 mkdir /app 
 VALIDATE $? "Creating directory"
-#else
-#echo -e "$Y File already exist $N"
+else
+echo -e "$Y File already exist $N"
 fi
 
 curl -L -o /tmp/cart.zip https://roboshop-builds.s3.amazonaws.com/cart.zip &>> $LOGFILE
