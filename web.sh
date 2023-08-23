aec30ebe6fc2c7598405dea991e6273206914f35
devi <devimeena.iiit@gmail.com>
2023-08-23 14:28:04 +0530

    creating web

---
+++ web.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+LOGFILE_DIR=/tmp
+DATE=$(date +%F)
+SCRIPT_NAME=$0
+LOGFILE=$LOGFILE_DIR/$SCRIPT_NAME-$DATE.log
+R="\e[31m"
+G="\e[32m"
+N="\e[0m"
+Y="\e[33m"
+VALIDATE(){
+    if [ $1 -ne 0 ]; then
+        echo -e "$2.....$R FAILURE $N"
+        exit 1
+    else
+        echo -e "$2.....$G SUCCESS $N"
+    fi
+ }
+
+ USERID=$(id -u)
+ if [ $USERID -ne 0 ]; then
+    echo -e "$R ERROR:Please run this script with root access $N"
+    exit 1
+fi
+
+yum install nginx -y &>> $LOGFILE
+VALIDATE $? "Installing nginx"
+
+systemctl enable nginx
+VALIDATE $? "Enabling nginx"
+
+systemctl start nginx
+VALIDATE $? "Starting nginx"
