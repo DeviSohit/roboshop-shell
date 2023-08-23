d2423d496ce458d1c28310473412718ca80d26b8
devi <devimeena.iiit@gmail.com>
2023-08-23 15:41:47 +0530

    commit mscript

--- redis.sh
+++ redis.sh
@@ -22,20 +22,26 @@ VALIDATE(){
     exit 1
 fi
 
-yum install https://rpms.remirepo.net/enterprise/remi-release-8.rpm -y &>> $LOGFILE
-VALIDATE $? "Installing redis rpm file"
+yum install https://rpms.remirepo.net/enterprise/remi-release-8.rpm -y &>>$LOGFILE
 
-yum module enable redis:remi-6.2 -y &>> $LOGFILE
-VALIDATE $? "Enabling the 6.2 version of redis"
+VALIDATE $? "Installing Redis repo"
 
-yum install redis -y &>> $LOGFILE
-VALIDATE $? "Installing REDIS"
+yum module enable redis:remi-6.2 -y &>>$LOGFILE
+
+VALIDATE $? "Enabling Redis 6.2"
+
+yum install redis -y &>>$LOGFILE
+
+VALIDATE $? "Installing Redis 6.2"
 
 sed -i 's/127.0.0.1/0.0.0.0/g' /etc/redis.conf /etc/redis/redis.conf &>>$LOGFILE
+
 VALIDATE $? "Allowing Remote connections to redis"
 
-systemctl enable redis &>> $LOGFILE
-VALIDATE $? "Enabling redis"
+systemctl enable redis &>>$LOGFILE
 
-systemctl start redis &>> $LOGFILE
-VALIDATE $? "Starting redis"
+VALIDATE $? "Enabling Redis"
+
+systemctl start redis &>>$LOGFILE
+
+VALIDATE $? "Starting Redis"
