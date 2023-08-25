122f97646cc8e3eddae3021b923a1e3cbd45b258
devi <devimeena.iiit@gmail.com>
2023-08-25 16:45:40 +0530

    creating multiple ec-2 instances

--- create-ec2.sh
+++ create-ec2.sh
@@ -1,22 +1,21 @@
 #!/bin/bash
 
-Names=("mongodb" "redis" "mysql" "rabbitmq" "catalogue" "user" "cart" "shipping" "payment" "dispatch" "web")
+NAMES=("mongodb" "redis" "mysql" "rabbitmq" "catalogue" "user" "cart" "shipping" "payment" "dispatch" "web")
 INSTANCE_TYPE=""
 IMAGE_ID=ami-03265a0778a880afb
-SECURITY_GROUP_ID=sg-0d784987bc4c86087
-DOMAIN_NAME=devidevops.online
+SECURITY_GROUP_ID=sg-0189d4870cc65fd55
+DOMAIN_NAME=joindevops.online
 
 # if mysql or mongodb instance_type should be t3.medium , for all others it is t2.micro
 
-for i in "{$Names[@]}"
-do
-    if [[ $i == mongodb || $i == mysql ]]
+for i in "${NAMES[@]}"
+do  
+    if [[ $i == "mongodb" || $i == "mysql" ]]
     then
         INSTANCE_TYPE="t3.medium"
     else
         INSTANCE_TYPE="t2.micro"
     fi
-
     echo "creating $i instance"
     IP_ADDRESS=$(aws ec2 run-instances --image-id $IMAGE_ID  --instance-type $INSTANCE_TYPE --security-group-ids $SECURITY_GROUP_ID --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$i}]" | jq -r '.Instances[0].PrivateIpAddress')
     echo "created $i instance: $IP_ADDRESS"
